69b1e9d305dac6c1b97e3ae470bf7e66815a658c
Aaron Winter <aaronvvinter@gmail.com>
2021-05-18 17:41:16 +0200

    Changed ro responsive design with Limit Screen Curve.

--- js/javascripts.js
+++ js/javascripts.js
@@ -1,6 +1,13 @@
 //Dokumentation
 $(document).ready(function(){   
         $('.worktitle').click(function(){
-            $(this).parent().next('.doc-text').slideToggle();
+            $(this).parent().next('.doc-text').slideToggle(200);
         });
 });
+
+$(document).ready(function(){   
+        $('.doc-text').click(function(){
+            $(this).slideUp(200);
+        });
+});
+	
